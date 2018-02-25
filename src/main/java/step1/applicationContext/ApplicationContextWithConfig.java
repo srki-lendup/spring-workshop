f5507f588d6362b8ec861786ca0d251f9d75f5ab
Girish Verma <girishv@thoughtworks.com>
2018-02-25 15:15:41 +0530

    Add exercise1: Understaanding Application Context

---
+++ src/main/java/step1/applicationContext/ApplicationContextWithConfig.java
@@ -0,0 +1,22 @@
+package step1.applicationContext;
+
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class ApplicationContextWithConfig {
+
+    public static void main( String[] args ) {
+        ApplicationContext context = new AnnotationConfigApplicationContext( AnnotationConfiguration.class );
+
+        Book book1 = context.getBean( Book.class );
+        System.out.println( "This is a demo using annotation based configuration" );
+        System.out.println("Title: " + book1.getTitle());
+        System.out.println("Price: " + book1.getPrice());
+        System.out.println("Author: " + book1.getAuthor().getFirstName() + " " + book1.getAuthor().getLastName());
+
+        Book book2 = context.getBean( Book.class );
+        System.out.println("Are Book1 and Book2 same? " + (book1 == book2));
+    }
+
+}
---
+++ src/main/java/step1/applicationContext/Author.java
@@ -0,0 +1,21 @@
+package step1.applicationContext;
+
+
+public class Author {
+
+    private String firstName;
+    private String lastName;
+
+    public Author( ) {
+        this.firstName = "Craig";
+        this.lastName = "Walls";
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+}
