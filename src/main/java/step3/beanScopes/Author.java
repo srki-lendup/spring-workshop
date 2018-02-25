72a5b9621fdf44ddf8b4b306fc47a35df6c060fc
Girish Verma <girishv@thoughtworks.com>
2018-02-25 18:28:55 +0530

    Add exercise3: Understanding Bean Scopes

---
+++ src/main/java/step3/beanScopes/Author.java
@@ -0,0 +1,27 @@
+package step3.beanScopes;
+
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
+
+import static org.springframework.beans.factory.config.ConfigurableBeanFactory.SCOPE_PROTOTYPE;
+
+@Component
+@Scope( SCOPE_PROTOTYPE )
+public class Author {
+
+    private String firstName;
+    private String lastName;
+
+    public Author( ) {
+        this.firstName = "Craig";
+        this.lastName = "Walls";
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+}
---
+++ src/main/java/step3/beanScopes/BeanScanConfiguration.java
@@ -0,0 +1,9 @@
+package step3.beanScopes;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan
+public class BeanScanConfiguration {
+}
