00e8393bd2cd6ae3c0433e3294feeda2c6db657b
Girish Verma <girish.verma026@gmail.com>
2018-03-03 16:20:43 +0530

    Add exercise09: Web application with Spring Boot

---
+++ src/main/java/step09/springMvcWithBoot/controller/BookController.java
@@ -0,0 +1,22 @@
+package step09.springMvcWithBoot.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import step09.springMvcWithBoot.model.Book;
+
+@RestController
+public class BookController {
+
+    private final Book book;
+
+    @Autowired
+    public BookController( final Book book ) {
+        this.book = book;
+    }
+
+    @RequestMapping(value = "/book")
+    public Book getBook() {
+        return book;
+    }
+}
---
+++ src/main/java/step09/springMvcWithBoot/model/Author.java
@@ -0,0 +1,23 @@
+package step09.springMvcWithBoot.model;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Author {
+
+    private String firstName;
+    private String lastName;
+
+    public Author( ) {
+        this.firstName = "Craig";
+        this.lastName = "Walls";
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+}
