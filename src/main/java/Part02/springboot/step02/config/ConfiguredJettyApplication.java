3c109dcd5ee645da7dda7ce3e134fa247e01d772
Matthias Kainer <mail@matthias-kainer.de>
2018-03-04 22:38:18 +0100

    Added first two examples for spring boot

---
+++ src/main/java/Part02/springboot/step02/config/ConfiguredJettyApplication.java
@@ -0,0 +1,27 @@
+package Part02.springboot.step02.config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+/*
+import org.springframework.boot.web.embedded.jetty.JettyServletWebServerFactory;
+import org.springframework.context.annotation.Bean;
+*/
+
+@SpringBootApplication
+public class ConfiguredJettyApplication {
+    public static void main( String[] args ) {
+        SpringApplication.run( ConfiguredJettyApplication.class, args );
+    }
+
+    /*@Bean
+    public JettyServletWebServerFactory jettyEmbeddedServletContainerFactory() {
+
+        JettyServletWebServerFactory jettyContainer =
+                new JettyServletWebServerFactory();
+
+        jettyContainer.setPort(9000);
+        return jettyContainer;
+    }*/
+}
+
+
